a4731088256ac94406eb3e43548f6fb8e4229a39
Denis Wapukha <dwapukha6@gmail.com>
2021-09-10 09:48:56 +0300

    Latested Version on Exams update

--- Examination/admin.py
+++ Examination/admin.py
@@ -3,20 +3,17 @@ from django import forms
 from  django.db.models import Sum, F
 from Examination.models import Exam_Result, Grade, Exam,Exam_Type
 from student.models import Student_Admission
+from django.contrib.contenttypes.admin import GenericTabularInline
 # Register your models here.
 
 
 @admin.register(Exam_Result)
 class AdminExamResults(admin.ModelAdmin):
-
     list_display = ('Admin_No','subject', 'Cat_One','Cat_Two','Average_Cat', 'Main_Marks', 'total', 'Grade')
+    list_filter=('subject','Admin_No' )
 
     def Admin_No(self, obj):
         return obj.Student_Adminssion.id
-
-
-
-
 
 
 @admin.register(Grade)
